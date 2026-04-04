78194b20e8fca969985b8fe86bdb30e3aaa9482f
LoGoTIP69 <pavelkanxx@gmail.com>
2026-04-04 14:49:43 +0300

    Feature: rewrite validate_certs with openssl.

--- qsh/crates/security/src/lib.rs
+++ qsh/crates/security/src/lib.rs
@@ -1,9 +1,8 @@
 use rcgen::{generate_simple_self_signed, CertifiedKey as RcgenCertifiedKey};
 use std::fs;
-use std::io::BufReader;
 use std::path::PathBuf;
-use rustls_pemfile::{certs, pkcs8_private_keys};
-use rustls::{Certificate, PrivateKey, ServerConfig};
+use openssl::pkey::PKey;
+use openssl::x509::X509;
 
 /// Класс создание и проверки сертификатов
 pub struct Security {
@@ -50,31 +49,16 @@ impl Security {
             return Err(format!("Private key file not found: {}", key_path.display()).into());
         }
 
-        let mut cert_file = BufReader::new(fs::File::open(&cert_path)?);
-        let mut key_file = BufReader::new(fs::File::open(&key_path)?);
+        let cert_pem = fs::read(&cert_path)?;
+        let key_pem = fs::read(&key_path)?;
 
-        let mut certs_pem = certs(&mut cert_file)?;
-        let mut keys_pem = pkcs8_private_keys(&mut key_file)?;
-        
-        // Проверка содержимого файлов
-        if certs_pem.is_empty() {
-            return Err("No certificates found in cert.pem".into());
+        let cert = X509::from_pem(&cert_pem)?;
+        let pkey = PKey::private_key_from_pem(&key_pem)?;
+
+        if cert.public_key()?.public_eq(&pkey) {
+            Ok(())
+        } else {
+            Err("Certificate and private key do not match.".into())
         }
-        if keys_pem.is_empty() {
-            return Err("No private keys found in privkey.pem".into());
-        }
-
-        let cert = Certificate(certs_pem.remove(0));
-        let key = PrivateKey(keys_pem.remove(0));
-
-        // Проверка соответствия ключа и сертификата через создание ServerConfig
-        ServerConfig::builder()
-            .with_safe_defaults()
-            .with_no_client_auth()
-            .with_single_cert(vec![cert], key)
-            .map_err(|e| format!("Certificate and key do not match: {}", e))?;
-
-        Ok(())
     }
 }
-
